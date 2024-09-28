82af50fff1d17a348fa38dff3360b6ff1d90608f
Simran Tamang <simrant009@gmail.com>
2024-09-28 16:44:23 +0545

    result fetched in the each student page

--- lib/models/Students_model.dart
+++ lib/models/Students_model.dart
@@ -1,29 +1,34 @@
+import 'ResultModel.dart';
+
 class Student {
+  // String? id;
   final String studentId;
   final String fullName;
   final String classAssigned;
   final String parentName;
   String? rollNumber;
-  String? imageUrl; // Add rollNumber as a nullable field
+  String? imageUrl;
+  List<Result>? results; // Add rollNumber as a nullable field
 
-  Student({
-    required this.studentId,
-    required this.fullName,
-    required this.classAssigned,
-    required this.parentName,
-    this.rollNumber,
-    this.imageUrl, // Include rollNumber in the constructor
-  });
+  Student(
+      {required this.studentId,
+      required this.fullName,
+      required this.classAssigned,
+      required this.parentName,
+      this.rollNumber,
+      this.imageUrl,
+      this.results // Include rollNumber in the constructor
+      });
 
   factory Student.fromJson(Map<String, dynamic> json) {
     return Student(
-      studentId: json['studentId'],
-      rollNumber: json['rollNumber'],
-      fullName: json['fullName'],
-      classAssigned: json['classAssigned'],
-      parentName:
-          json['parentID'] != null ? json['parentID']['fullName'] : 'Unknown',
-      imageUrl: json['imageUrl'],
-    );
+        studentId: json['_id'],
+        rollNumber: json['rollNumber'],
+        fullName: json['fullName'],
+        classAssigned: json['classAssigned'],
+        parentName:
+            json['parentID'] != null ? json['parentID']['fullName'] : 'Unknown',
+        imageUrl: json['imageUrl'],
+        results: []);
   }
 }
--- lib/services/studentService.dart
+++ lib/services/studentService.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:eskool/constants/constants.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/ResultModel.dart';
 import '../models/Students_model.dart';
 
 class StudentService {
@@ -16,6 +17,19 @@ class StudentService {
       return data.map((studentJson) => Student.fromJson(studentJson)).toList();
     } else {
       throw Exception('Failed to load students');
+    }
+  }
+
+  static Future<List<Result>> fetchResultsByStudentId(String studentId) async {
+    final response =
+        await http.get(Uri.parse('$baseUrl/result/student-results/$studentId'));
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      return data.map((resultJson) => Result.fromJson(resultJson)).toList();
+    } else {
+      throw Exception('Failed to load results');
     }
   }
 }
