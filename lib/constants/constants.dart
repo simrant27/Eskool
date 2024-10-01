4e4d7ee6c0023fd0aea94168b5966d7d493b5675
Simran Tamang <simrant009@gmail.com>
2024-10-01 10:06:55 +0545

    changes

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -17,10 +17,10 @@ const red = Colors.red;
 //default app padding
 const appPadding = 16.0;
 
-const String url = 'http://192.168.2.227:3000';
+const String url = 'http://192.168.101.253:3000';
 const String ip = '192.168.2.227';
 const NoticeImage = "${url}/notice_upload";
 const TeacherImage = "${url}/uploads/teacher_upload";
-const ParentImage = "${url}/parent_upload";
+const ParentImage = "${url}uploads/parent_upload";
 const userTeacherImg = "$url/uploads/teacher_upload";
 const userParentImg = "$url/uploads/parent_upload";
